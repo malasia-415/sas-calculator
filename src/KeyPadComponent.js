ece24cefa5ea6699d47aee9f5b2eddc083720f35
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 17:26:53 -0700

    added = . and 0

--- src/KeyPadComponent.js
+++ src/KeyPadComponent.js
@@ -25,5 +25,12 @@ class KeyPadComponent extends Component {
                 <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
                 <button name="*" onClick={e => this.props.onClick(e.target.name)}>x</button><br/>
 
-                
+                <button name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
+                <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+                <button name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button><br/>
+            </div>
+        );
+    }
+}
 
