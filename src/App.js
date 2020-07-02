2baf7017a3df2e93d765556a2aed587eb62b95f0
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 17:36:34 -0700

    Format correction

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import ResultComponent from './components/ResultComponent';
 import KeyPadComponent from "./components/KeyPadComponent";
 
 class App extends Component {
-  constructor(){
-      super();
+    constructor(){
+        super();
 
-      this.state = {
-          result: ""
-      }
-  }
+        this.state = {
+            result: ""
+        }
+    }
 
-  render() {
-    return (
-        <div>
-            <div className="calculator-body">
-                <h1>Simple Calculator</h1>
-                <ResultComponent result={this.state.result}/>
-                <KeyPadComponent onClick={this.onClick}/>
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Simple Calculator</h1>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-}
+
 export default App;
