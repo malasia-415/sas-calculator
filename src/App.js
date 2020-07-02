b2079b2aa4b1ddcb7f5f57da4d4c260bfd4c5312
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-01 17:33:49 -0700

    render class name and connect the RC com. and the KPC

--- src/App.js
+++ src/App.js
@@ -11,25 +11,16 @@ class App extends Component {
       }
   }
 
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+        <div>
+            <div className="calculator-body">
+                <h1>Simple Calculator</h1>
+                <ResultComponent result={this.state.result}/>
+                <KeyPadComponent onClick={this.onClick}/>
+            </div>
+        </div>
+    );
 }
-
+}
 export default App;
